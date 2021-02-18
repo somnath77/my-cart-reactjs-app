e803db53abaa4f0d45e888a04e3bf5726929c198
Somnath <Somnath.surwase@joshsoftware.com>
2021-02-19 02:44:25 +0530

    my-cart, First react app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import * as lib from './cart'
+import productList from './productList.json'
+// console.log(productList);
+// import ProductList from './cart'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      < lib.ProductList />
+      {/* <lib.Danger/> */}
+      {/* <a href="www.reactjs.org"><p id="demo"></p></a> */
+
+        // productList.map( (product) => {
+        //     return <h2>{product.name}</h2>
+        // })
+      
+      }
+  
+      
+
+
     </div>
   );
 }
---
+++ src/cart.js
@@ -0,0 +1,69 @@
+import {Button} from 'reactstrap'
+import React ,{useState}from "react"
+import productList from './productList.json'
+import Productcard from './productcard'
+
+
+export const ProductList = () => {
+    return (
+        <>
+        {
+            productList.map( (product) => <Productcard product={product}/>)
+        }
+        </>
+    )
+}
+
+// export default ProductList
+
+
+// export function Danger() { 
+//     let [list,setList] = useState([])
+//     const handleClick = ()=>{
+//         list = productList.map(({name,id})=><div>
+//             <p>{name}</p>
+//             <p>{id}</p>
+//         </div>
+//         )
+//         setList(list)
+//     }
+//     // console.log(list)
+//     return (
+
+//         <>
+//         <Button onClick={handleClick}>Click me </Button>
+//         <div>
+//             <ul>
+//                 {list.length !== 0 && list}
+//             </ul>
+//         </div>
+//         </>
+//     );
+// }
+
+// function functionOnClick() {
+//     return(
+//     <div className="App">
+//         {
+//             // productList.map( ({name,id}) => 
+//             //     <h5 key={id}>  {name} </h5>
+//             // )
+//             productList.map( (product) => {
+//                 return <h2>{product['name']}</h2>
+//             })
+//         }
+//     </div>
+       
+        
+//     );
+//     // function handleClick(e) {
+//     //     e.preventDefault();
+//     //     console.log('The link was clicked.');
+//     //   }
+    
+//     //   return (
+//     //     <a href="#" onClick={handleClick}>
+//     //       Click me
+//     //     </a>
+//     //   );
+// }
